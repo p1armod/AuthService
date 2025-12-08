518bf5f48b0ec6ccb2647c46560eace41edbbc7b
p1armod <parmodrns@gmail.com>
2025-12-08 13:13:00 +0530

    Add AppConfig for modelmapper

---
+++ src/main/java/org/auth/Config/AppConfig.java
@@ -0,0 +1,17 @@
+package org.auth.Config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
